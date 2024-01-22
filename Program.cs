ea2beb7c96fa45db0237971ca170d8bc87546b7d
David <davidvillegasr@gmail.com>
2024-01-22 11:38:31 -0500

    added instructions for the work

--- Program.cs
+++ Program.cs
@@ -6,8 +6,46 @@ namespace _2023_12_29_Menu
     {
         public static void Main(string[] args)
         {
-            //Aqui comienza nuestro codigo para desarrollar una agencia de
-            //viajes que plantea un cierto menu.
+            /*
+             * deffinion du problemme
+             * Algorithmes et structures de données
+             * Projets
+               Equipe de 2 ou individuel
+               
+               Projet  
+               Pour les besoins de gestion d’une agence de voyage, on veut un système qui nous 
+               permet d’offrir des infos aux clients potentiels.
+               Chaque offre du voyagiste est composée des forfaits suivants : destination, transport, 
+               hébergement et loisir.
+               La destination indique seulement le nom de la ville alors que le transport peut être 
+               par air, mer ou terrestre et pour chacun on ne liste que le prix total, durée du 
+               voyage et le nom de la compagnie. Pour l’hébergement, on ne listera que le nombre 
+               d’étoiles de l’hôtel, le prix de la nuitée et le nom de l’hôtel. Finalement, pour 
+               les loisirs, on ne liste que la description du loisir et le coût associé.
+               Un client appelle le voyagiste et fait une réservation. On notera alors le nom, prénom 
+               et numéro de téléphone du client et l’offre qu’il a choisie. 
+               Le gérant veut lister à la fin de la journée les offres qu’il a vendues. Écrire 
+               l’algorithme qui donne la liste des clients (nom, prénom, num téléphone) ainsi que 
+               l’offre choisie.
+                            
+               BESOINS
+               Proposer une démarche (algorithme) pour la saisie et le stockage des données 
+               ainsi que l'affichage éventuel des informations
+               
+               Traduire l’algorithme en C#. 
+               On prévoira un système de menu pour le projet.
+                         
+               Recommandations importantes
+               
+               Vous devez fournir un découpage approprié de la solution choisie.
+               L’entête de l'algorithme doit comporter une description du problème en utilisant vos propres mots.  
+               
+               
+             * Deffinition des structures de données
+             * 
+             */
+            //Debut de notre programme pour une agence de voyage
+            //Voici le menu.
             bool exit = true;
             do
             {
